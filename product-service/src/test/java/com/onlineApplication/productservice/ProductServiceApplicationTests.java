3da4160bb57c18437f1270efb217aa187ced8689
Prashanth69116 <121217213+Prashanth69116@users.noreply.github.com>
2023-08-04 15:04:09 +0530

    Add files via upload
    first Commit

---
+++ product-service/src/test/java/com/onlineApplication/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineApplication.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
